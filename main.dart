d9d3ebf408585fb263d5a431973c2cd3fab8d8de
SandhyaThapa3691 <thapasandhya1301@gmail.com>
2024-07-20 02:37:38 +0545

    Weather App

---
+++ main.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: unused_import
+
+import 'package:flutter/material.dart';
+import 'package:weatherapp/home.dart';
+import 'package:weatherapp/loading.dart';
+import 'package:weatherapp/location.dart';
+void main() {
+  runApp(const MaterialApp(
+    home: HomePage(),
+  ));
+}
