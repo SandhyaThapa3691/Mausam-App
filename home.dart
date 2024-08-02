1cd092abf00325d00af23e8afda44b3ae0a7ca19
SandhyaThapa3691 <thapasandhya1301@gmail.com>
2024-08-02 21:32:42 +0545

    Weather App

--- home.dart
+++ home.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
+
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({super.key, required Sapnam});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  void timer() {
+    Future.delayed(const Duration(seconds: 8), () {
+      print("Alarm is Ringing");
+    });
+    print("Alarm is nice");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Text("Home Activity"),
     );
   }
--- loading.dart
+++ loading.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:weatherapp/home.dart';
+
 class LoadingPage extends StatefulWidget {
-  const LoadingPage({super.key});
+  final String Sapnam;
+  LoadingPage({super.key, required this.Sapnam});
 
   @override
   State<LoadingPage> createState() => _LoadingPageState();
@@ -10,7 +13,21 @@ class _LoadingPageState extends State<LoadingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("loading Activity"),
-    );
+        body: SafeArea(
+      child: Column(
+        children: [
+          TextButton.icon(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const HomePage(Sapnam: "sapnam"),
+                    ));
+              },
+              icon: const Icon(Icons.add_to_home_screen),
+              label: const Text("Go To Home"))
+        ],
+      ),
+    ));
   }
 }
